cee88b6eafaab6b69500814b1e74065f2bbbf8f4
Gihan Jayakuru <gihan22603@gmail.com>
2023-08-06 22:41:12 +0530

    v4 Backend:Products and Categories

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
-app.get(api+'/p',(req, res) => {
-    res.send('Heloo API GET');
+
+app.use(cors());
+app.options('*', cors())
+//midleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+
+//Routes
+const productsRoutes = require('./routes/products');
+const categoriesRoutes = require('./routes/categories');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+app.use(`${api}/products`, productsRoutes)
+app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/users`, usersRoutes)
+// app.use(`${api}/orders`, ordersRoutes)
+
+
+
+// app.get(`${api}/products`,(req, res) => {
+//     const product = {
+//         id:1,
+//         name:'dress1aa',
+//         des:'sdff'
+//     }
+//     res.send(product);
+// })
+
+// app.post(`${api}/products`,(req, res) =>{
+//     const newProduct = req.body;
+//     console.log(newProduct);
+//     res.send(newProduct);
+// })
+
+ // Database connection
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log('Database Connection is ready....')
+})
+.catch((err)=>{
+    console.log(err);
 })
 
-
+// server
 app.listen(3000, ()=>{
     console.log('server is running http://localhost:3000'+api);
 })
